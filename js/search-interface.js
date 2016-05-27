d15fe3a8184851bf2ba13309c1726b6dd3a1c921
SamDurfey <samuel.durfey@gmail.com>
2016-05-27 13:06:37 -0700

    Create list of user's repos, all of which are clickable links that work and stuff.

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,20 +1,24 @@
 var apiKey = require("./../.env").apiKey;
-var getRepoList = require("./../js/search.js").getRepoList;
+var getUserStuff = require("./../js/search.js").getUserStuff;
+var printRepoList = require("./../js/search.js").printRepoList;
 
 $(document).ready(function() {
+
+
 	// As a user, when I search for a user name, I want to be able to see a list of their repos,
 	// populated inside of a list...
 	$("#userSearchForm").submit(function(event){
 		event.preventDefault();
+		$("#searchResults").empty();
 		var userName = $("#searchUser").val();
-		getRepoList(userName);
+		printRepoList(userName);
+		// getUserStuff(userName);
 
 		// populate populate list with results
-
-		$("#resultsContainer").slideDown(); // reveal results div.
+		$("#resultsContainer").slideDown();
 	});
 
-	var repolist = getRepoList("samdurfey");
-	console.log("hello " + repolist);
+	// debugger;
 
 });
+
--- js/search.js
+++ js/search.js
@@ -1,9 +1,19 @@
 var apiKey = require("./../.env").apiKey;
 
-exports.getRepoList = function(userName) {
-	return $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
+exports.getUserStuff = function(userName) {
+	$.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
 		console.log(response);
-    return response;
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
+exports.printRepoList = function(userName) {
+	$.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
+		console.log(response);
+    for (var i = 0; i < response.length; i++) {
+    	$("#searchResults").append("<li><a href='" + response[i].html_url + "'>" + response[i].name + "</a></li>");
+    }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
