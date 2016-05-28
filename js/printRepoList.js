7a6f71d125dece8d031c6940c81c01316a80abed
SamDurfey <samuel.durfey@gmail.com>
2016-05-27 20:52:55 -0700

    Fix styling to no longer be strange.

--- js/printRepoList.js
+++ js/printRepoList.js
@@ -3,13 +3,16 @@ exports.printRepoList = function(url) {
 	$.get(url).then(function(response){
 		console.log(response);
     for (var i = 0; i < response.length; i++) {
-    	$("#repoList").append("<li><a href='" + response[i].html_url + "'>" + response[i].name + ":</a></li>");
-    	if ( response[i].description === "") {
-    		$("#repoDescriptions").append("<li>...</li>");
-    	} else {
-    		$("#repoDescriptions").append("<li>" + response[i].description + "</li>");
-    	}
-    	
+    	$("#repoList").append(
+        '<li><div class="row">' + 
+          '<div class="col-xs-4">' + 
+            '<p><a href="' + response[i].html_url + '">' + response[i].name + '......</a></p>' +
+          '</div>' +
+          '<div class="col-xs-8">' +
+            '<p>' + response[i].description + '</p>' + 
+          '</div>'+
+        '</div></li>'
+        );
     }
   }).fail(function(error){
     console.log(error.responseJSON.message);
